3c1538698a007a16167b18cf4158e5786031db54
Misael97001 <chiluisajefferson97@gmail.com>
2026-05-03 18:39:25 -0500

    Clase Carta y Dealer

---
+++ src/com/krakedev/juegos/servicios/Dealer.java
@@ -0,0 +1,46 @@
+package com.krakedev.juegos.servicios;
+
+import java.util.ArrayList;
+import com.krakedev.juegos.entidades.Carta;
+
+public class Dealer {
+
+    private ArrayList<Carta> naipe;
+
+    public Dealer() {
+        naipe = new ArrayList<Carta>();
+        generarNaipe();
+    }
+
+    public ArrayList<Carta> getNaipe() {
+        return naipe;
+    }
+
+    public void setNaipe(ArrayList<Carta> naipe) {
+        this.naipe = naipe;
+    }
+
+    public void generarNaipe() {
+
+        String[] valores = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        String[] palos = {"T","CN","CR","D"};
+
+        for (String palo : palos) {
+            for (String valor : valores) {
+
+                Carta c = new Carta();
+                c.setValor(valor);
+                c.setPalo(palo);
+
+                naipe.add(c);
+            }
+        }
+    }
+
+    public void imprimirNaipe() {
+        for (Carta c : naipe) {
+            c.imprimir();
+        }
+    }
+
+}
