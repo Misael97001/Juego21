3398e99b1d6e685f64f3eb2a84e5a1238a0d239e
Misael97001 <chiluisajefferson97@gmail.com>
2026-05-03 18:45:03 -0500

    Crear clase jugador

---
+++ src/com/krakedev/juegos/entidades/Jugador.java
@@ -0,0 +1,34 @@
+package com.krakedev.juegos.entidades;
+
+import java.util.ArrayList;
+
+public class Jugador {
+
+    private String nickname;
+    private ArrayList<Carta> cartas = new ArrayList<Carta>();
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void recibirCarta(Carta c) {
+        cartas.add(c);
+    }
+
+    public ArrayList<Carta> getCartas() {
+        return cartas;
+    }
+
+    public void imprimir() {
+
+        System.out.println("Jugador: " + nickname);
+
+        for (Carta c : cartas) {
+            c.imprimir();
+        }
+    }
+}
